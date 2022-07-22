6d0dc3d9998c6b1a30e96de1646ce2667ca49c27
Henrique Dias Silva <henrique.dias@rede.ulbra.br>
2022-07-22 15:53:22 -0300

    Segunda parte do tutorial Django

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,11 +74,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'tutodjango', 
+        'USER': 'admin', 
+        'PASSWORD': '1234', 
+        'HOST': '127.0.0.1', 
+        'PORT': '5432', 
+    } 
 }
 
 
