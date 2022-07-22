8d00e613259a545cc71f60d55eeeac3c2641b9f1
Henrique Dias Silva <henrique.dias@rede.ulbra.br>
2022-07-22 17:02:22 -0300

    Terceira parte do tutorial Django

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,6 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = { 
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': { 
         'ENGINE': 'django.db.backends.postgresql', 
         'NAME': 'tutodjango', 
